eb014e4aa6380ce056dae37bde3b996e6a07704f
liyicky <jason@liyicky.com>
2023-01-23 20:28:57 +0900

    Initial Commit

---
+++ PickerWithCoreData/PickerWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerWithCoreDataApp.swift
+//  PickerWithCoreData
+//
+//  Created by Jason Cheladyn on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PickerWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
